f43346df9645cf8c55030d7e3227464ae1ea38d9
maggie <margaret13.may@gmail.com>
2023-10-28 18:35:16 -0500

    run server.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 /** Database connection for messagely. */
 
 const { Client } = require("pg");
-const { DB_URI } = require("./config");
+// const { DB_URI } = require("./config");
 
 // const client = new Client(DB_URI);
 
