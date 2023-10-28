5ecea58c49da9147c02112fbd24d567bf916cef5
maggie <margaret13.may@gmail.com>
2023-10-28 15:58:36 -0500

    modify db.js

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 /** Database connection for messagely. */
 
-
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
-const client = new Client(DB_URI);
+// const client = new Client(DB_URI);
 
-client.connect();
+let db = new Client({
+  host: "/var/run/postgresql/",
+  database: "messagely",
+});
 
+db.connect();
 
-module.exports = client;
+module.exports = db;
